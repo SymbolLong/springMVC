bfca1084f296bfad56783e23a399291ab3738d75
SymbolLong <877857078@qq.com>
2017-09-21 16:32:42 +0800

    配置访问jsp

---
+++ src/main/java/com/zhang/spring/config/WebInitializer.java
@@ -0,0 +1,24 @@
+package com.zhang.spring.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+
+public class WebInitializer implements WebApplicationInitializer{
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(MyConfig.class);
+        context.setServletContext(servletContext);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatch",
+                new DispatcherServlet(context));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
---
+++ src/main/resources/views/index.jsp
@@ -0,0 +1,13 @@
+<%@page language="java" pageEncoding="UTF-8" contentType="text/html; UTF-8" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0.1 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>首页</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+    <pre>
+        Welcome to Spring MVC World!
+    </pre>
+</body>
+</html>
